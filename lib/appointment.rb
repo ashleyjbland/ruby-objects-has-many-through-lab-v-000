053f4110faf2a8dd3fe6ab273293152d36db7dca
Ashley Bland <ashleyjbland@gmail.com>
2018-09-25 22:12:06 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,3 +1,20 @@
 class Appointment
+  attr_accessor :date
+  @@all = []
+
+  def initialize(date, patient, doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 
 end
+
+The Appointment class needs a class variable @@all that begins as an empty array.
+The Appointment class needs a class method .all that lists each appointment in the class variable.
+An appointment should be initialized with a date (as a string, like "Monday, August 1st"), a patient, and a doctor. The appointment should be saved in the @@all array.
