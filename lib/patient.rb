cd02926d76128dbbd24b772cb3bdb1c276b527db
Ashley Bland <ashleyjbland@gmail.com>
2018-09-25 22:16:20 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -15,15 +15,19 @@ class Patient
     Appointment.new(date, self, doctor)
   end
 
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.patient == self
+    end
+  end
+
+  def doctors
+    Appointment.all.collect do |appointment|
+      appointment.doctor
+    end
+  end
+
 end
-
-
-
-The Patient class needs an instance method, new_appointment, that takes in an argument of a doctor
-and a date and creates a new appointment. The appointment should know that it belongs to the patient.
-
-The Patient class needs an instance method, appointments, that iterates through the appointments array
-and returns appointments that belong to the patient.
 
 The Patient class needs an instance method, doctors, that iterates over that patient's appointments
 and collects the doctor that belongs to each appointment.
