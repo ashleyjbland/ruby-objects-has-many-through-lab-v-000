aa0d4858f2d948c38c955478c587b14b36a3d289
Ashley Bland <ashleyjbland@gmail.com>
2018-09-25 21:44:52 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -16,10 +16,16 @@ class Genre
       song.genre == self
     end
   end
+
+  def artists
+    Song.all.select do |song|
+      song.artist
+    end
+  end
+  
 end
 
-The Genre class needs an instance method,
-songs, that iterates through all songs and finds the songs that belong to that genre.
+
 
 The Genre class needs an instance method,
 artists, that iterates over the genre's collection of songs and collects the artist that owns each song.
