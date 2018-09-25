e97aa842f3e7c3147d8e8c33d2f4e9338f003242
Ashley Bland <ashleyjbland@gmail.com>
2018-09-25 22:12:13 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -14,7 +14,3 @@ class Appointment
   end
 
 end
-
-The Appointment class needs a class variable @@all that begins as an empty array.
-The Appointment class needs a class method .all that lists each appointment in the class variable.
-An appointment should be initialized with a date (as a string, like "Monday, August 1st"), a patient, and a doctor. The appointment should be saved in the @@all array.
